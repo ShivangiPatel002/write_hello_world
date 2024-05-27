9b04336d3629d0e09881703c3bf0fc737170d8d9
Shivangi Patel <shivangi@mobioxy.com>
2024-05-27 13:05:06 +0530

    Made some changes

--- test/write_hello_world_test.dart
+++ test/write_hello_world_test.dart
@@ -1,12 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
 
-import 'package:write_hello_world/write_hello_world.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-  });
+  // test('adds one to input values', () {
+  //   final calculator = Calculator();
+  //   expect(calculator.addOne(2), 3);
+  //   expect(calculator.addOne(-7), -6);
+  //   expect(calculator.addOne(0), 1);
+  // });
 }
